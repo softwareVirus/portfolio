d4a8f1b1bcd201f72061d77fc8d4eed9c62ad9ec
softwareVirus <ibrahimsdakli52@hotmail.com>
2024-05-10 22:46:26 +0300

    Refactor import to link for font awesome and google fonts

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,22 @@ import { useState } from "react";
 import "./globals.css"; // Your global styles
 import { Providers } from "./providers";
 import StepSidebar from "@/components/StepSidebar";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:400,300"
+        />
+      </Head>
       <body>
         {/* Edges for the progress steps */}
         <Providers>
