800dd7afaec588a9f9df6f7f9e933c889d8d932f
softwareVirus <ibrahimsdakli52@hotmail.com>
2024-05-11 03:37:10 +0300

    Fix css

--- components/BottomNavigation.jsx
+++ components/BottomNavigation.jsx
@@ -35,7 +35,7 @@ const BottomNavigation = () => {
           onClick={() => handlePageChange(index)}
           className={`${selectedStep === index ? "active" : ""}`}
           animate={{
-            scale: selectedStep == index ? 1.1 : 1,
+            scale: selectedStep == index ? 1 : 0.8,
           }}
           transition={{duration: 0.2}}
         >
