b3b9e826c3fc20235523857009e37843cbb6e324
softwareVirus <ibrahimsdakli52@hotmail.com>
2024-05-13 00:50:52 +0300

    Create particle options

---
+++ util/particle.js
@@ -0,0 +1,50 @@
+export default {
+  background: {
+    color: {
+      value: "#000",
+    },
+  },
+  fpsLimit: 120,
+  particles: {
+    color: {
+      value: "var(--selected-theme-main-color)",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
